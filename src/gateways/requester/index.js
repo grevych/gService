634bfc7c9a33aaa059b26c7ff19b49a4042d5858
Gerardo Reyes <grevych@gmail.com>
2017-12-15 11:06:27 -0600

    Add requester gateway base class  - Include request method  - Include retry method using exponential backoff strategy

---
+++ src/gateways/requester/index.js
@@ -0,0 +1,46 @@
+const sprintf = require('sprintf');
+const retry = require('retry-as-promised');
+
+const Gateway = require('../../gateways');
+
+const ERROR_FAILED_ON_REQUEST = 'Failed request to: %s%s';
+
+
+const RETRY_EXPONENTIAL_BACKOFF_SETTINGS = {
+  max: 3,
+  match: [],
+  timeout: 60000,
+  backoffBase: 500,
+  backoffExponent: 1.3,
+};
+
+
+class Requester extends Gateway {
+  constructor(service) {
+    super(service);
+    this.useRetry = this.service.useRetry;
+
+    RETRY_EXPONENTIAL_BACKOFF_SETTINGS.match.push(this.service.retryError);
+  }
+
+  request(path, options) {
+    if (this.useRetry) {
+      return this.retryRequest(path, options);
+    }
+    return this.service.request(path, options);
+  }
+
+  retry(path, options) {
+    const { baseURL } = this.service;
+
+    return retry(() => this.service.request(path, options),
+                 RETRY_EXPONENTIAL_BACKOFF_SETTINGS)
+      .then(response => response)
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+}
+
+
+module.exports = Requester;
